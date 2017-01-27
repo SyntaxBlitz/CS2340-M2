15348f3ca85c7236102c382bec4fcd57d174e250
Aayush Kumar <aayush.kumarmail@gmail.com>
2017-01-27 00:14:41 -0500

    Person 4 changed by aayush

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Aayush Kumar
  *  @version 1.1
  */
 public class Person4 {
@@ -31,6 +31,9 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
+      String output = "";
+      for (int i = 0; i < input.length(); i++) output += (input.charAt(i) + 1);
+      System.out.println(output);
       return null;
     }
     
